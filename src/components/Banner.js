d4d09cf9797d60c3528e64430e8351e80eba8d57
Thaiza Capelão <thaizacapelao@gmail.com>
2023-08-02 14:54:35 -0400

    Adicionando bibliotecas, imgs e criando funcao para movimentacoes

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,18 +1,70 @@
-import { Container } from "react-bootstrap";
+import { useState, useEffect } from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import { ArrowRightCircle } from "react-bootstrap-icons";
+import headerImg from "../assets/img/header-img.png";
 
 export const Banner = () => {
+  const [loopNum, setLoopNum] = useState(0);
+  const [isDeleting, setIsDeleting] = useState(false);
+  const toRotate = [" Front-End Developer", " Web Designer", " UI/UX Designer"];
+  const [text, setText] = useState('');
+  const [delta, setDelta] = useState(300 - Math.random() * 100);
+  const period = 2000;
+
+  useEffect(() => {
+    let ticker = setInterval(() => {
+      tick();
+    }, delta);
+
+    return () => {
+      clearInterval(ticker);
+    };
+  }, [text, delta]);
+
+  const tick = () => {
+    let i = loopNum % toRotate.length;
+    let fullText = toRotate[i];
+    let updatedText = isDeleting
+      ? fullText.substring(0, text.length - 1)
+      : fullText.substring(0, text.length + 1);
+
+    setText(updatedText);
+
+    if (isDeleting) {
+      setDelta((prevDelta) => prevDelta / 2);
+    }
+
+    if (!isDeleting && updatedText === fullText) {
+      setIsDeleting(true);
+      setDelta(period);
+    } else if (isDeleting && updatedText === "") {
+      setIsDeleting(false);
+      setLoopNum(loopNum + 1);
+      setDelta(500);
+    }
+  };
+
   return (
     <section className="banner" id="home">
       <Container>
-        <row className="align-items-center">
-          <col xs={12} md={6} xl={7}>
-            <span className="tagline">Bem vindo ao meu Portifolio</span>
+        <Row className="align-items-center">
+          <Col xs={12} md={6} xl={7}>
+            <span className="tagline">Bem vindo ao meu Portfólio</span>
             <h1>
-              {`Olá eu sou uma Desenvolvedora Web`}
-              <span className="wrap">Front-end Developer</span>
+              {"Olá, eu sou uma Desenvolvedora web "}
+              <span className="wrap">{text}</span>
             </h1>
-          </col>
-        </row>
+            <p>
+              Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has be
+            </p>
+            <button onClick={() => console.log("connect")}>
+              Conecte-se comigo!<ArrowRightCircle size={25} />
+            </button>
+          </Col>
+          <Col xs={12} md={6} xl={5}>
+            <img src={headerImg} alt="Imagem do Header" />
+          </Col>
+        </Row>
       </Container>
     </section>
   );
