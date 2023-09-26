578fc2db61841077b9de6bf2f683c0060f9b653a
Thaiza Capelão <thaizacapelao@gmail.com>
2023-09-26 15:23:46 -0400

    Adicionando validacoes, mensagem de erro e atualizando a parte de Contato do portifolio

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,29 +1,109 @@
-import { useState } from "react"
+import { useState } from "react";
 import { Container } from "react-bootstrap";
 import contactImg from "../assets/img/contact-img-svg";
 
 export const Contactc = () => {
-        const formInitialDetails = {
-            firstName: '',
-            lastName: '',
-            email: '',
-            phone: '',
-            message: '',
-        }
-        const [formDetails, setFormDetails] = useState(formInitialDetails);
-        const [buttonText, setButtonText] = useState('Send');
-        const [status, setStatus] = useState({});
+  const formInitialDetails = {
+    firstName: "",
+    lastName: "",
+    email: "",
+    phone: "",
+    message: "",
+  };
+  const [formDetails, setFormDetails] = useState(formInitialDetails);
+  const [buttonText, setButtonText] = useState("Send");
+  const [status, setStatus] = useState({});
 
+  const onFormUpdate = (category, value) => {
+    setFormDetails({
+      ...formDetails,
+      [category]: value,
+    });
+  };
 
-        return (
-        <section className="contact" id="connect">
-            <Container>
-              <Row ClassName="align-items-center">
-                <col md={6}>
-                    <img src ={contactImg} alt="Entre em contato comigo"/>
-                </col>
-                </Row>  
-            </Container>
-        </section>
-    )
-}
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setButtonText('Sending...');
+    let response = await fetch("http://localhost:5000/contact", {
+        method:"POST",
+        headers: {
+            "Content-Type" : "Application/json;charset=utf-8",
+        },
+        body: JSON.stringify(formDetails),
+    });
+    setButtonText("Send");
+    let result = response.json();
+    setFormDetails(formInitialDetails);
+  }
+
+  return (
+    <section className="contact" id="connect">
+      <Container>
+        <Row ClassName="align-items-center">
+          <Col md={6}>
+            <img src={contactImg} alt="Entre em contato comigo" />
+          </Col>
+          <Col md={6}>
+            <h2>Get In Touch</h2>
+            <form onSubmit={handleSubmit}>
+              <Row>
+                <Col sm={6} className="px-1">
+                  <input
+                    type="text"
+                    value={formDetails.firstName}
+                    placeholder="Primeiro Nome"
+                    onChange={(e) => onFormUpdate("firstName", e.target.value)}
+                  />
+                </Col>
+                <Col sm={6} className="px-1">
+                  <input
+                    type="text"
+                    value={formDetails.lastName}
+                    placeholder="Ultimo Nome"
+                    onChange={(e) => onFormUpdate("lastName", e.target.value)}
+                  />
+                </Col>
+                <Col sm={6} className="px-1">
+                  <input
+                    type="email"
+                    value={formDetails.email}
+                    placeholder="Endereço de Email"
+                    onChange={(e) => onFormUpdate("email", e.target.value)}
+                  />
+                </Col>
+                <Col sm={6} className="px-1">
+                  <input
+                    type="tel"
+                    value={formDetails.phone}
+                    placeholder="Número de Telefone"
+                    onChange={(e) => onFormUpdate("phone", e.target.value)}
+                  />
+                </Col>
+                <Col>
+                  <textarea
+                    row="6"
+                    value={formDetails.message}
+                    placeholder="Mensagem"
+                    onChange={(e) => onFormUpdate("message", e.target.value)}
+                  />
+                  <button type="submit">
+                    <span>{buttonText}</span>
+                  </button>
+                </Col>
+                {status.message && (
+                  <Col>
+                    <p
+                      className={status.sucess === false ? "danger" : "sucess"}
+                      >
+                      {status.message}
+                    </p>
+                  </Col>
+                )}
+              </Row>
+            </form>
+          </Col>
+        </Row>
+      </Container>
+    </section>
+  );
+};
